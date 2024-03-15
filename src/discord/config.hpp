e347bcf67ae3571d5c599665bf666713e2359e3d
awaわ <a970565@163.com>
2024-03-15 22:14:47 +0800

    Implementation of read slahcommand

--- src/discord/config.hpp
+++ src/discord/config.hpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 
+
 //初始设定
 class config {
 private:
@@ -11,10 +12,17 @@ public:
 	std::string get_token()const {
 		return token;
 	}
+private:
+	void read_token();
+	std::vector<std::pair<std::string, std::string>> read_slashcommand();
 };
 //在调用config时就将设定填满,没有的返回 -1 
 config::config()
 {
+	read_token();
+}
+
+void config::read_token() {
 	std::ifstream stream("/home/awalwa/projects/bot/config.txt");
 	std::string line;
 	while (getline(stream, line)) {
@@ -23,4 +31,17 @@ config::config()
 			token = line;
 		}
 	}
+}
+
+inline std::vector<std::pair<std::string, std::string>> config::read_slashcommand() {
+	std::ifstream stream("/home/awalwa/projects/bot/slashcommand.txt");
+	std::string line;
+	std::vector<std::pair<std::string, std::string>> cmd_content;
+	std::string tmp;
+	while (getline(stream, line)) {
+		tmp = line;
+		getline(stream, line);
+		cmd_content.push_back(std::pair<std::string, std::string>() = { tmp,line });
+	}
+	return cmd_content;
 }
