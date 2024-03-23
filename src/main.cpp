fa086f2ddf41dcfca18069b941f888af9ac94a37
awaわ <a970565@163.com>
2024-03-24 00:04:47 +0800

    reconfiguration

--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "discord/awa-koto.h"
-#include "discord/config.hpp"
-//main内主要运行on_Fn的函数popen
-
-int main() {
-	config Ru_config;
-	awa_koto Ru_koto(Ru_config.get_token(), dpp::st_wait);
-}
