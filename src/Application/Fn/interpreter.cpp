225fdaf2232e283273a79b15e852cd9c91ed6b96
awaわ <a970565@163.com>
2024-05-18 00:14:41 +0800

    大清洗

--- src/Application/Fn/interpreter.cpp
+++
@@ -1,9 +0,0 @@
-#include "../Ru_koto.h"
-
-#include <stdlib.h>
-
-namespace Rult {
-	void Ru_koto::interpreter() {
-
-	}
-}
--- src/Application/default.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#define TERMINAL_NUM 0
-#define SLASHCOMMAND_NUM 1
