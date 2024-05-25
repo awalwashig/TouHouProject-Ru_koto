a95799e0762f0a2dc2a20fc2137ed0897dee81d5
awaわ <a970565@163.com>
2024-05-25 15:25:36 +0800

    史

--- src/Ru_koto.cpp
+++ src/Ru_koto.cpp
@@ -1,1 +1,3 @@
 #include "Ru_koto.h"
+
+
--- src/Ru_koto.h
+++ src/Ru_koto.h
@@ -2,12 +2,31 @@
 #define _RU_KOTO_H_
 #pragma once
 
+#include <iostream>
+#include <vector>
+#include <queue>
 /*
 该类主要设计成为多个框架提供传递的方法
 目前来说是这样的
 */
 namespace RuKoto {
-
+	//
+	class UniversalContent;
+	class Config;
+	class Message;
+	class Slashcommand;
 }
 
+class RuKoto::UniversalContent {
+
+private:
+
+};
+
+class RuKoto::Config {
+public:
+	void SetUniversalContent(UniversalContent* m_class);
+private:
+	RuKoto::UniversalContent* m_UniversalContent;
+};
 #endif // !RU_KOTO_H
